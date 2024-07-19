68f31e1fca577f348f0db612d3cf156dc33091c4
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2024-07-19 09:59:20 +0800

    fixing the deploy url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+import "leaflet/dist/leaflet.css";
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon, divIcon, point } from "leaflet";
